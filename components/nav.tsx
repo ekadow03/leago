ac81983012a73195883a704a736342f68f7e45cd
Evan <ekadow03@gmail.com>
2026-07-13 14:39:52 -0700

    Increase logo size in nav

--- components/nav.tsx
+++ components/nav.tsx
@@ -6,7 +6,7 @@ export default function Nav() {
   return (
     <nav className="nav">
       <Link href="/" style={{ display: 'flex', alignItems: 'center' }}>
-        <Image src="/leago-logo.png" alt="leago" width={120} height={36} style={{ height: 28, width: 'auto' }} priority />
+        <Image src="/leago-logo.png" alt="leago" width={160} height={48} style={{ height: 40, width: 'auto' }} priority />
       </Link>
     </nav>
   );
