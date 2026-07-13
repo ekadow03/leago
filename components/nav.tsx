105d5fd253445d76641250757db77a5a6d037c36
Evan <ekadow03@gmail.com>
2026-07-13 14:28:59 -0700

    Add real logo/icon assets, update Nav to use them

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,11 +1,12 @@
 // components/nav.tsx
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Nav() {
   return (
     <nav className="nav">
-      <Link href="/register" className="nav-logo">
-        lea<span>go</span>
+      <Link href="/" style={{ display: 'flex', alignItems: 'center' }}>
+        <Image src="/leago-logo.png" alt="leago" width={120} height={36} style={{ height: 28, width: 'auto' }} priority />
       </Link>
     </nav>
   );
